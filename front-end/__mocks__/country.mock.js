a2263487c94206ea0178e9db8466eddf61aeba62
Lionel Adams <adamslionel@hotmail.co.uk>
2019-08-22 00:07:10 +0100

    data: Add mock data for countries and country (gb, pt)

---
+++ front-end/__mocks__/country.mock.js
@@ -0,0 +1,33 @@
+export const pt = {
+  name: 'Portugal',
+  native: 'Portugal',
+  currency: 'EUR',
+  phone: '351',
+  languages: [
+    {
+      code: 'pt',
+      name: 'Portuguese',
+      native: 'Português',
+      rtl: null,
+    },
+  ],
+  emoji: '🇵🇹',
+  emojiU: 'U+1F1F5 U+1F1F9',
+};
+
+export const gb = {
+  name: 'United Kingdom',
+  native: 'United Kingdom',
+  currency: 'GBP',
+  phone: '44',
+  languages: [
+    {
+      code: 'en',
+      name: 'English',
+      native: 'English',
+      rtl: null,
+    },
+  ],
+  emoji: '🇬🇧',
+  emojiU: 'U+1F1EC U+1F1E7',
+};
