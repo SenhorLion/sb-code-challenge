725531723e038952bc56d799936af5e2767ebb79
Lionel Adams <adamslionel@hotmail.co.uk>
2019-08-22 21:20:14 +0100

    style: Test styled-component

--- front-end/src/routes/Home.js
+++ front-end/src/routes/Home.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+// TODO: Move to a styled component folder "typography or summink"
+const StyledH1 = styled.h1`
+  font-size: 4em;
+  color: ${props => props.theme.colors.primary};
+`;
 
 /**
  * Display basic links for the other routes
@@ -7,7 +14,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <StyledH1>Home</StyledH1>
 
       <ul>
         <li>
