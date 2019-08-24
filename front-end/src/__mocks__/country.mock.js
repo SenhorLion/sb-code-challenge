5e2e85cd9de377662ced164ebaa7e70c68eea5c9
Lionel Adams <adamslionel@hotmail.co.uk>
2019-08-24 23:45:42 +0100

    test: Add mock data

--- front-end/src/__mocks__/country.mock.js
+++ front-end/src/__mocks__/country.mock.js
@@ -31,3 +31,26 @@ export const gb = {
   emoji: '🇬🇧',
   emojiU: 'U+1F1EC U+1F1E7',
 };
+
+export const as = {
+  name: 'American Samoa',
+  native: 'American Samoa',
+  currency: 'USD',
+  phone: '1684',
+  languages: [
+    {
+      code: 'en',
+      name: 'English',
+      native: 'English',
+      rtl: null,
+    },
+    {
+      code: 'sm',
+      name: 'Samoan',
+      native: 'Gagana Samoa',
+      rtl: null,
+    },
+  ],
+  emoji: '🇦🇸',
+  emojiU: 'U+1F1E6 U+1F1F8',
+};
