5e2d52197ecbf69482d55312d6836baea3902348
Lionel Adams <adamslionel@hotmail.co.uk>
2019-08-25 20:38:24 +0100

    feat: Add container styled-components

---
+++ front-end/src/components/containers/index.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const ContainerDiv = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  margin-bottom: 6px;
+  padding: 0.5em;
+`;
+export const HeaderDiv = styled.div`
+  flex-direction: row;
+  justify-content: center;
+  align-content: left;
+  padding: 1em;
+`;
+
+export const ErrorDiv = styled.div`
+  display: flex;
+  flex: 1;
+  border: 0px solid ${props => props.theme.colors.black};
+  padding: 0.5em;
+  color: ${props => props.theme.colors.secondaryMagenta};
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+`;
