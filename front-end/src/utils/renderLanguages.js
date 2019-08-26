3d478fb016299321eb8556afee59f69337c79aa1
Lionel Adams <adamslionel@hotmail.co.uk>
2019-08-26 01:01:54 +0100

    refactor: Add styled-component for languages

--- front-end/src/utils/renderLanguages.js
+++ front-end/src/utils/renderLanguages.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Lang = styled.code`
+  display: block;
+  color: ${props => props.theme.colors.coolGey};
+  font-weight: 400;
+  padding-bottom: 0.75em;
+`;
 
 const renderLanguages = (languages = []) =>
   languages.map(lang => {
     return (
-      <p key={lang.code}>
+      <Lang key={lang.code}>
         {lang.name} ({lang.native})
-      </p>
+      </Lang>
     );
   });
 
